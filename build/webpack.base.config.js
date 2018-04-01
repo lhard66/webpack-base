e09c23b0ee90155b50b71f46e9ea4d3e4e3f599f
Allen Liu <lhard@qq.com>
2018-04-01 19:43:50 +0800

    css的基础配置，并提取css样式为文件

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,14 +1,30 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const baseWebpackConfig = {
   entry: {
-    'main': './src/main',
+    'main': './src/main.js',
   },
   output: {
     path: path.join(__dirname, '../dist'), // 要求为绝对路径
     publicPath: '/dist/', // 对于按需加载(on-demand-load)或加载外部资源(external resources)（如图片、文件等）来说，output.publicPath 是很重要的选项。如果指定了一个错误的值，则在加载这些资源时会收到 404 错误。
     filename: 'main.js', // 每个输出 bundle 的名称。如：filename: "[name].[hash].bundle.js"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader'],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader'],
+        }),
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css'),
+  ],
 }
 
 module.exports = baseWebpackConfig;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
+import '../style/main.css';
+
 document.getElementById('num').innerHTML = 'hello worl000000000d!!9999!';
